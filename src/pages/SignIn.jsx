37b793770ed76176ba69af65c9710a4d89d856ab
sholabioku <bilushonline@gmail.com>
2022-05-08 11:41:21 +0100

    Add SiginIn and SiginUp forms

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,75 @@
-import React from 'react';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
+import visibilityIcon from '../assets/svg/visibilityIcon.svg';
 
 const SignIn = () => {
-  return <div>Sign In</div>;
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  return (
+    <>
+      <div className='pageContainer'>
+        <header>
+          <p className='pageHeader'>Welcome Back!</p>
+        </header>
+        <form>
+          <input
+            type='email'
+            id='email'
+            value={email}
+            placeholder='Email'
+            className='emailInput'
+            onChange={handleChange}
+          />
+          <div className='passwordInputDiv'>
+            <input
+              type={showPassword ? 'text' : 'password'}
+              className='passwordInput'
+              id='password'
+              placeholder='Password'
+              value={password}
+              onChange={handleChange}
+            />
+            <img
+              src={visibilityIcon}
+              alt='show password'
+              className='showPassword'
+              onClick={() => setShowPassword((prevState) => !prevState)}
+            />
+          </div>
+          <Link to='/forgot-password' className='forgotPasswordLink'>
+            Forgot Password
+          </Link>
+
+          <div className='signInBar'>
+            <p className='signInText'>Sign In</p>
+            <button className='signInButton'>
+              <ArrowRightIcon fill='#fff' width='34px' height='34px' />
+            </button>
+          </div>
+        </form>
+        {/* Google OAuth */}
+
+        <Link to='/sign-up' className='registerLink'>
+          Sign Up Instead
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default SignIn;
