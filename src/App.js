5c16644dae4c1ed1087c96c06a869b77bd4ed62b
sholabioku <bilushonline@gmail.com>
2022-05-09 11:59:02 +0100

    Add functionality to logout user

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './components/Navbar';
 import Explore from './pages/Explore';
