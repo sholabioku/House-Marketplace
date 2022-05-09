1afb33c7780a345229baa40e446c2dd8382c7c46
sholabioku <bilushonline@gmail.com>
2022-05-09 15:35:47 +0100

    Add clean up functionality for memory leak

--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -1,20 +1,27 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 export const useAuthStatus = () => {
+  const isMounted = useRef(true);
   const [loggedIn, setLoggedIn] = useState(false);
   const [checkingStatus, setCheckingStatus] = useState(true);
 
   useEffect(() => {
-    const auth = getAuth();
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setLoggedIn(true);
-      }
+    if (isMounted) {
+      const auth = getAuth();
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setLoggedIn(true);
+        }
 
-      setCheckingStatus(false);
-    });
-  });
+        setCheckingStatus(false);
+      });
+    }
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, [isMounted]);
 
   return { loggedIn, checkingStatus };
 };
