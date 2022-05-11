0cc39105eb56375981dad220fa2ea33b311098bc
sholabioku <bilushonline@gmail.com>
2022-05-11 12:56:52 +0100

    Add functionality for fetch listings from firebase

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,9 +1,80 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from 'firebase/firestore';
+import { db } from '../firebase.config';
+import { toast } from 'react-toastify';
+import Spinner from '../components/Spinner';
 
 const Category = () => {
+  const params = useParams();
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        // Get reference to the collection
+        const listingRef = collection(db, 'listings');
+
+        // Create a query
+        const q = query(
+          listingRef,
+          where('type', '==', params.categoryName),
+          orderBy('timestamp', 'desc'),
+          limit(10)
+        );
+
+        // Execute the query
+        const querySnap = await getDocs(q);
+
+        const listings = [];
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+        setListings(listings);
+        setLoading(false);
+      } catch (error) {
+        toast.error('Could not fetch listings');
+      }
+    };
+    fetchListings();
+  }, [params.categoryName]);
+
   return (
-    <div>
-      <h1>Category</h1>
+    <div className='category'>
+      <header>
+        <p className='pageHeader'>
+          {params.categoryName === 'rent'
+            ? 'Places for rent'
+            : 'Places for sale'}
+        </p>
+      </header>
+      {loading ? (
+        <Spinner />
+      ) : listings && listings.length > 0 ? (
+        <>
+          <main>
+            <ul className='categoryListings'>
+              {listings.map((listing) => (
+                <h3 key={listing.id}>{listing.data.name}</h3>
+              ))}
+            </ul>
+          </main>
+        </>
+      ) : (
+        <p>No listings for {params.categoryName}</p>
+      )}
     </div>
   );
 };
