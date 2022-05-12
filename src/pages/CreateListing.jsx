b7d48b78ff0ab1b2a8fa935da3fc4493c90d0b0c
sholabioku <bilushonline@gmail.com>
2022-05-12 18:31:55 +0100

    Add functionality to uploading images in firebase

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,6 +1,14 @@
 import { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from 'firebase/storage';
+import { db } from '../firebase.config';
+import { v4 as uuidv4 } from 'uuid';
 import Spinner from '../components/Spinner';
 import { toast } from 'react-toastify';
 
@@ -63,17 +71,18 @@ const CreateListing = () => {
 
   const onSubmit = async (e) => {
     e.preventDefault();
+
     setLoading(true);
 
     if (discountedPrice >= regularPrice) {
       setLoading(false);
-      toast.error('Discounted price must be less than regular price');
+      toast.error('Discounted price needs to be less than regular price');
       return;
     }
 
     if (images.length > 6) {
       setLoading(false);
-      toast.error('You can only upload 6 images');
+      toast.error('Max 6 images');
       return;
     }
 
@@ -97,15 +106,64 @@ const CreateListing = () => {
 
       if (location === undefined || location.includes('undefined')) {
         setLoading(false);
-        toast.error('Invalid address');
+        toast.error('Please enter a correct address');
+        return;
       }
-      return;
     } else {
       geolocation.lat = latitude;
       geolocation.lng = longitude;
-      location = address;
     }
 
+    // Store image in firebase
+    const storeImage = async (image) => {
+      return new Promise((resolve, reject) => {
+        const storage = getStorage();
+        const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
+
+        const storageRef = ref(storage, 'images/' + fileName);
+
+        const uploadTask = uploadBytesResumable(storageRef, image);
+
+        uploadTask.on(
+          'state_changed',
+          (snapshot) => {
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is ' + progress + '% done');
+            switch (snapshot.state) {
+              case 'paused':
+                console.log('Upload is paused');
+                break;
+              case 'running':
+                console.log('Upload is running');
+                break;
+              default:
+                break;
+            }
+          },
+          (error) => {
+            reject(error);
+          },
+          () => {
+            // Handle successful uploads on complete
+            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              resolve(downloadURL);
+            });
+          }
+        );
+      });
+    };
+
+    const imgUrls = await Promise.all(
+      [...images].map((image) => storeImage(image))
+    ).catch(() => {
+      setLoading(false);
+      toast.error('Images not uploaded');
+      return;
+    });
+
+    console.log(imgUrls);
     setLoading(false);
   };
 
